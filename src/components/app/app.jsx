a7a1d75c749f0580562f539fa1cff150f2d64d9d
Nick <nickmiloff@users.noreply.github.com>
2021-02-04 15:33:33 +0300

    Исправляет замечания

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../header/header';
-import Main from '../main/main';
+import MainPage from '../pages/main-page/main-page';
 
 const App = ({cards}) => {
   return (
     <div className="page page--gray page--main">
       <Header />
-      <Main cards={cards} />
+      <MainPage cards={cards} />
     </div>
   );
 };
---
+++ src/mock.js
@@ -0,0 +1,32 @@
+export const cards = [
+  {
+    id: 1,
+    isFavorite: false,
+    isPremium: false,
+    previewImage: `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/15.jpg`,
+    price: 136,
+    rating: 3.5,
+    title: `Canal View Prinsengracht`,
+    type: `room`
+  },
+  {
+    id: 2,
+    isFavorite: false,
+    isPremium: true,
+    previewImage: `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/3.jpg`,
+    price: 222,
+    rating: 4.3,
+    title: `Penthouse, 4-5 rooms + 5 balconies`,
+    type: `room`
+  },
+  {
+    id: 3,
+    isFavorite: false,
+    isPremium: true,
+    previewImage: `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/17.jpg`,
+    price: 202,
+    rating: 3.3,
+    title: `he Joshua Tree House`,
+    type: `apartment`
+  }
+];
