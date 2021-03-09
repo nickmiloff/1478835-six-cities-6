bdc3234ac397d6dff9ec8adf7bdbdfb8ccfed4a8
Nick <nickmiloff@users.noreply.github.com>
2021-03-09 17:23:56 +0300

    Изменяет тесты для redirect middleware

--- src/store/middlewares/redirect.test.js
+++ src/store/middlewares/redirect.test.js
@@ -1,17 +1,53 @@
 import {redirect} from './redirect';
-import * as types from './types';
 import * as actions from './actions';
 
-describe(`Middlewares async operation work correctly`, () => {
-  it(`Should correctly redirect to '/'`, () => {
-    const next = jest.fn();
+const mockRedux = () => {
+  const store = {
+    getState: jest.fn(() => ({})),
+    dispatch: jest.fn(),
+  };
 
-    redirect(null)(next)(actions.redirectToRoute(`/`));
+  const next = jest.fn();
+  const invoke = (action) => redirect(store)(next)(action);
+  return {store, next, invoke};
+};
 
-    expect(next).toHaveBeenCalledTimes(1);
-    expect(next).toHaveBeenNthCalledWith(1, {
-      type: types.REDIRECT_TO_ROUTE,
-      payload: `/`,
-    });
+const fakeHistory = {
+  location: {pathname: ``},
+  push(path) {
+    this.location.pathname = path;
+  }
+};
+
+jest.mock(`../../browser-history`, () => fakeHistory);
+
+describe(`Redirect async operation work correctly`, () => {
+  it(`Should correctly pass to next middleware`, () => {
+    const {invoke, next} = mockRedux();
+
+    const action = actions.redirectToRoute(`/`);
+
+    invoke(action);
+    expect(next).toHaveBeenCalledWith(action);
+  });
+
+  it(`Should correctly add route to fakeHistory`, () => {
+    const {invoke} = mockRedux();
+    const url = `/url`;
+
+    const action = actions.redirectToRoute(url);
+
+    invoke(action);
+    expect(fakeHistory.location.pathname).toBe(url);
+  });
+
+  it(`Shouldn't redirect because bad action`, () => {
+    const {invoke} = mockRedux();
+    const url = `/bad-url`;
+
+    const incorrectAction = {type: ``, payload: url};
+
+    invoke(incorrectAction);
+    expect(fakeHistory.location.pathname).not.toBe(url);
   });
 });
