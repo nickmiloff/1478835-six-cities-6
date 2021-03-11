16fdf2a687e5ecc7ca4e36acc26f8c0a1a8eccdc
Nick <nickmiloff@users.noreply.github.com>
2021-03-11 04:09:08 +0300

    Разбивает на чанки

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import {Route, Switch} from 'react-router-dom';
-import OfferPage from '../pages/offer-page/offer-page';
-import FavoritesPage from '../pages/favorites-page/favorites-page';
-import LoginPage from '../pages/login-page/login-page';
-import MainPage from '../pages/main-page/main-page';
-import NotFoundPage from '../pages/not-found-page/not-found-page';
 import PrivateRoute from '../private-route/private-route';
+
+import Spiner from '../spiner/spiner';
+
+const OfferPage = lazy(() => import(`../pages/offer-page/offer-page`));
+const FavoritesPage = lazy(() => import(`../pages/favorites-page/favorites-page`));
+const LoginPage = lazy(() => import(`../pages/login-page/login-page`));
+const MainPage = lazy(() => import(`../pages/main-page/main-page`));
+const NotFoundPage = lazy(() => import(`../pages/not-found-page/not-found-page`));
 
 export const ROUTES = {
   offer: `/offer`,
@@ -16,13 +19,15 @@ export const ROUTES = {
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path={`${ROUTES.offer}/:id`} component={OfferPage} />
-      <PrivateRoute exact path={ROUTES.favorites} component={FavoritesPage} />
-      <Route exact path={ROUTES.login} component={LoginPage} />
-      <Route exact path={ROUTES.main} component={MainPage} />
-      <Route component={NotFoundPage} />
-    </Switch>
+    <Suspense fallback={<Spiner/>}>
+      <Switch>
+        <Route exact path={`${ROUTES.offer}/:id`} component={OfferPage} />
+        <PrivateRoute exact path={ROUTES.favorites} component={FavoritesPage} />
+        <Route exact path={ROUTES.login} component={LoginPage} />
+        <Route exact path={ROUTES.main} component={MainPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Suspense>
   );
 };
 
--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,61 +1,68 @@
 import '@testing-library/jest-dom';
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import {render, screen, waitFor, cleanup} from '@testing-library/react';
 import Test from '../test/test';
 
 import App, {ROUTES} from './app';
 
+afterEach(cleanup);
+
 describe(`Test routing`, () => {
-  it(`Render 'Main page' when user navigate to '/' url`, () => {
+  const timeout = 1000;
+
+  it(`Render 'Main page' when user navigate to '/' url`, async () => {
     render(
         <Test>
           <App />
         </Test>
     );
 
-    expect(screen.getByText(`Cities`)).toBeInTheDocument();
+    await waitFor(() => expect(screen.getByText(`Cities`)).toBeInTheDocument(), {timeout});
   });
 
-  it(`Render 'Login page' when user navigate to '/login' url`, () => {
+  it(`Render 'Login page' when user navigate to '/login' url`, async () => {
     render(
         <Test pushUrl={ROUTES.login}>
           <App />
         </Test>
     );
 
-    expect(!!screen.getAllByText(`Sign in`)).toBe(true);
+    await waitFor(() => expect(!!screen.getAllByText(`Sign in`)).toBe(true), {timeout});
   });
 
-  it(`Render 'Favorites page' when user navigate to '/favorites' url`, () => {
+  it(`Render 'Favorites page' when user navigate to '/favorites' url`, async () => {
     render(
         <Test pushUrl={ROUTES.favorites} initial={false}>
           <App />
         </Test>
     );
 
-    expect(screen.getByText(`Saved listing`)).toBeInTheDocument();
+    await waitFor(() => expect(screen.getByText(`Saved listing`)).toBeInTheDocument(), {timeout});
   });
 
-  it(`Render 'OfferPage page' when user navigate to '/offer/:id' url`, () => {
+  it(`Render 'OfferPage page' when user navigate to '/offer/:id' url`, async () => {
     render(
         <Test pushUrl={`${ROUTES.offer}/:id`} initial={false}>
           <App />
         </Test>
     );
 
-    expect(screen.getByText(`Tile House`)).toBeInTheDocument();
-    expect(screen.getByText(`Discover daily local life in city center, friendly neighborhood, clandestine casino, karaoke, old-style artisans, art gallery and artist studio downstairs.`)).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByText(`Tile House`)).toBeInTheDocument();
+      expect(screen.getByText(`Discover daily local life in city center, friendly neighborhood, clandestine casino, karaoke, old-style artisans, art gallery and artist studio downstairs.`)).toBeInTheDocument();
+    }, {timeout});
   });
 
-
-  it(`Render 'Not found page' when user navigate to non-existent url`, () => {
+  it(`Render 'Not found page' when user navigate to non-existent url`, async () => {
     render(
         <Test pushUrl="/non-existent-url">
           <App />
         </Test>
     );
 
-    expect(screen.getByText(`404 Page not found`)).toBeInTheDocument();
-    expect(screen.getByText(`Home page`)).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByText(`404 Page not found`)).toBeInTheDocument();
+      expect(screen.getByText(`Home page`)).toBeInTheDocument();
+    }, {timeout});
   });
 });
